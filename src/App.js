f3482ef56013235e097fd8900c6afc0aa7bdfe0b
Joao Vitor <joao-vitor.fede@hpe.com>
2023-04-04 21:04:01 -0300

    configuração inicio do projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+        <Row>
+           PROJETO
+        </Row>
+        
+    </div>
+  );
+}
+
+export default App;
